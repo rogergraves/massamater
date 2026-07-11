5933c31763a6de1178320eb4c7858ecb8937c3e0
Roger Graves <rogergraves@gmail.com>
2026-07-11 18:39:37 +0100

    fix(store-hours): update all 7 days on save, not just submitted ones

--- app/controllers/staff/store_hours_controller.rb
+++ app/controllers/staff/store_hours_controller.rb
@@ -6,12 +6,14 @@ class Staff::StoreHoursController < Staff::BaseController
   end
 
   def update
-    (params[:store_hours] || {}).each do |id, attrs|
-      hour = StoreHour.find(id)
-      open = attrs[:open] == "1"
-      hour.update!(open: open,
-                   opens_at:  attrs[:opens_at],
-                   closes_at: attrs[:closes_at])
+    hours_params = params[:store_hours] || {}
+    StoreHour.order(:day_of_week).each do |hour|
+      attrs = hours_params[hour.id.to_s] || {}
+      hour.update!(
+        open:      attrs[:open] == "1",
+        opens_at:  attrs[:opens_at].presence || hour.opens_at,
+        closes_at: attrs[:closes_at].presence || hour.closes_at
+      )
     end
     redirect_to edit_staff_store_hours_path, notice: t("store_hours.saved")
   end
